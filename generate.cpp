879d5a2ad5ede958e3ee332c480aa5bbdd23ce05
ChrisKau <uhchris04@gmail.com>
2024-03-02 14:01:13 -0800

    created files

---
+++ generate.cpp
@@ -0,0 +1,23 @@
+// Author: Chris Kau
+// CECS 325-02
+// Sorting Contest (Bubble Sort - Prog 3)
+// Due 03/12/2024
+// I certify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int arc, char *argv[])
+{
+	if (argc < 4)
+	{
+		cout << "Please enter 3 parameters:\n";
+		cout << "	Parameter 1: total count of numbers you want";
+		cout << "	Parameter 2: min value.\n";
+		cout << "	Parameter 3: max value.\n";
+		cout << "Example: %generate 10000000 100000 999999\n";
+		exit(EXIT_SUCCESS);
+	}
+
+	for (int i = argv[1]; i <= )
+}
---
+++ mysort.cpp
@@ -0,0 +1,5 @@
+// Author: Chris Kau
+// CECS 325-02
+// Sorting Contest (Bubble Sort - Prog 3)
+// Due 03/12/2024
+// I certify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
